7fd9af71f96a91479b0b0f07b14defda91b9cced
Jake Simmens <rocknjake@gmail.com>
2020-02-19 15:45:06 -0600

    prepare to merge with main branch

--- app.js
+++ app.js
@@ -7,25 +7,30 @@ app.use(express.static("public"));
 //Pass in url variable to allow navbar to update the active page
 
 app.get("/", (req, res) => {
-    var url = req.url;
-    res.render("home", {url: url});
+    let url = req.url;
+    res.render("home", { url: url });
 });
 
 app.get("/about", (req, res) => {
-    var url = req.url;
-    res.render("about", {url: url});
+    let url = req.url;
+    res.render("about", { url: url });
 });
 
 app.get("/services", (req, res) => {
-    var url = req.url;
-    res.render("services", {url: url});
+    let url = req.url;
+    res.render("services", { url: url });
 });
 
 app.get("/contact", (req, res) => {
-    var url = req.url;
-    res.render("contact", {url: url});
+    let url = req.url;
+    res.render("contact", { url: url });
 });
 
-app.listen(3000, () => {
-    console.log("AW Server listening on 3000");
+// app.listen(3000, () => {
+//     console.log("AW Server listening on 3000");
+// });
+
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("AW server running");
 });
